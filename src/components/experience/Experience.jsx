7c7db501189f53c80740aae1394baabae039cf29
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-17 22:08:16 -0500

    add flex-chip to job description

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,92 +1,50 @@
-import React from 'react'
-import '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
-import 'react-vertical-timeline-component/style.min.css'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
-import './Experience.css'
+import React from 'react';
+import '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import 'react-vertical-timeline-component/style.min.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import Chip from '@mui/material/Chip';
+import './Experience.css';
+import work_history from './work_history_data';
 
 const Experience = () => {
-return (
-    <div className='experience'>
-            <h1>Experience</h1>
-            <div className="timeline">
-                <VerticalTimeline className="timeline-vertical" lineColor='linear-gradient(180deg, #f83f0b 10%, #571183 100%)'>
-                    <VerticalTimelineElement
-                            className="job"
-                            contentStyle={{ }}
-                            contentArrowStyle={{ borderRight: '10px solid  white' }}
-                            textClassName='job-description'
-                            dateClassName='job-date'
-                            date="May 2014 – Dec 2017"
-                            iconClassName='job-icon'
-                            iconStyle={{ background: 'linear-gradient(270deg, #f83f0b 10%, #571183 100%' }}
-                            icon={<FontAwesomeIcon icon={faBriefcase} />}
-                    >
-                            <h3 className="job-title">Data Analyst</h3>
-                            <h4 className="job-city">Vaughan, ON</h4>
-                            <p>
-                                <ul>
-                                        <li>Designed and developed dashboards and reports</li> 
-                                        <li>Build weekly and monthly sales and financial reports</li>
-                                        <li>Developed machine learning models customer segmentation, market segmentation, sales forecasting</li>
-                                        <li>Developed customer segmentation models based on various attributes</li>
-                                </ul>
-                            </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                            className="job"
-                            contentStyle={{ }}
-                            contentArrowStyle={{ borderRight: '10px solid  white' }}
-                            textClassName='job-description'
-                            dateClassName='job-date'
-                            date="May 2014 – Dec 2017"
-                            iconClassName='job-icon'
-                            iconStyle={{ color: 'white', background: 'linear-gradient(270deg, #f83f0b 10%, #571183 100%)' }}
-                            icon={<FontAwesomeIcon icon={faBriefcase} />}
-                    >
-                            <h3 className="job-title">Technology Consultant</h3>
-                            <h4 className="job-city">Dallas, TX</h4>
-                            <p>
-                            Full-stack Web Development, GenAI/LLM, Project Management, Business Development
-                            </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                            className="job"
-                            contentStyle={{ }}
-                            contentArrowStyle={{ borderRight: '10px solid  white' }}
-                            textClassName='job-description'
-                            dateClassName='job-date'
-                            date="May 2014 – Dec 2017"
-                            iconClassName='job-icon'
-                            iconStyle={{ color: 'white', background: 'linear-gradient(270deg, #f83f0b 10%, #571183 100%)' }}
-                            icon={<FontAwesomeIcon icon={faBriefcase} />}
-                    >
-                            <h3 className="job-title">Technology Consultant</h3>
-                            <h4 className="job-city">Dallas, TX</h4>
-                            <p>
-                            Full-stack Web Development, GenAI/LLM, Project Management, Business Development
-                            </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                            className="job"
-                            contentStyle={{ }}
-                            contentArrowStyle={{ borderRight: '10px solid white' }}
-                            textClassName='job-description'
-                            dateClassName='job-date'
-                            date="May 2014 – Dec 2017"
-                            iconClassName='job-icon'
-                            iconStyle={{ color: 'white', background: 'linear-gradient(270deg, #f83f0b 10%, #571183 100%)' }}
-                            icon={<FontAwesomeIcon icon={faGraduationCap} />}
-                    >
-                            <h3 className="job-title">Seneca College</h3>
-                            <h4 className="job-city">Toronto, ON</h4>
-                            <p></p>
-                    </VerticalTimelineElement>
-                </VerticalTimeline>
-            </div>
-    </div>
-)
-}
+        const reversedWorkHistory = [...work_history].reverse(); 
+        return (
+                <div className="experience">
+                <h1>Experience</h1>
+                <div className="timeline">
+                        <VerticalTimeline className="timeline-vertical" lineColor="linear-gradient(180deg, #f83f0b 10%, #571183 100%)">
+                        {reversedWorkHistory && reversedWorkHistory.length > 0 ? (
+                                reversedWorkHistory.map((job_history, index) => (
+                                <VerticalTimelineElement
+                                        key={job_history.id || index} // Use `id` if available, fallback to `index`
+                                        className="job"
+                                        contentArrowStyle={{ borderRight: '10px solid white' }}
+                                        textClassName="job-description"
+                                        dateClassName="job-date"
+                                        date={job_history.date}
+                                        iconStyle={{ background: 'linear-gradient(270deg, #f83f0b 10%, #571183 100%)' }}
+                                        icon={<FontAwesomeIcon icon={faBriefcase} />}
+                                >
+                                        <h3 className="job-title">{job_history.title}</h3>
+                                        <h4 className="job-city">{job_history.city}</h4>
+                                        <p className="job-duties">{job_history.description}</p>
+                                        <div className="job-tool-flex-chips">
+                                        {job_history.tools &&
+                                                job_history.tools.map((tool, index) => (
+                                                <Chip key={index} className="job-tool-chip" label={tool} />
+                                                ))}
+                                        </div>
+                                </VerticalTimelineElement>
+                                ))
+                        ) : (
+                                <p>No work history available.</p>
+                        )}
+                        </VerticalTimeline>
+                </div>
+                </div>
+        );
+};
 
-export default Experience
+export default Experience;
