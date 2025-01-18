efe35130a599b2e1ab28eb9c957c83872e0489a9
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-17 22:09:52 -0500

    add work_history file

---
+++ src/components/experience/work_history_data.js
@@ -0,0 +1,37 @@
+
+const work_history = [
+    {
+        id: 1,
+        title: 'Data Analyst',
+        city: 'Vaughan, ON',
+        date: "May 2014 – Dec 2017",
+        description: ['Data Modeling', 'Dashboards', 'Metric reports', 'Statistical Analysis', 'Script Automation', 'Customer segmentation', 'Market segmentation', 'Sales forecasting', 'Demand forecasting'],
+        tools: ['Tableau', 'MySQL', 'Excel', 'Pandas']
+    },
+    {
+        id: 2,
+        title: 'Data Analyst',
+        city: 'Vaughan, ON',
+        description: ['Data Cleaning', 'Data Modeling', 'Statistical Analysis', 'Script Automation', 'Dashboards', 'Metric reports', 'Customer segmentation', 'Market segmentation', 'Sales forecasting', 'Demand forecasting'],
+        date: "May 2014 – Dec 2017",
+        tools: ['Tableau', 'MySQL', 'Excel', 'Pandas']
+    },
+    {
+        id: 3,
+        title: 'Data Analyst',
+        city: 'Vaughan, ON',
+        description: ['Data Cleaning', 'Data Modeling', 'Statistical Analysis', 'Script Automation', 'Dashboards', 'Metric reports', 'Customer segmentation', 'Market segmentation', 'Sales forecasting', 'Demand forecasting'],
+        date: "May 2014 – Dec 2017",
+        tools: ['Tableau', 'MySQL', 'Excel', 'Pandas']
+    },
+    {
+        id: 4,
+        title: 'Seneca College',
+        city: 'Toronto, ON',
+        description: ['Data Cleaning', 'Data Modeling', 'Statistical Analysis', 'Script Automation', 'Dashboards', 'Metric reports', 'Customer segmentation', 'Market segmentation', 'Sales forecasting', 'Demand forecasting'],
+        date: "May 2014 – Dec 2017",
+        tools: ['Tableau', 'MySQL', 'Excel', 'Pandas']
+    }
+];
+
+export default work_history;
