29a9ef7bb2b8234da01f3f49263a1bb0f3938268
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-18 18:02:47 -0500

    add attributes to footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareGithub, faLinkedin, faKaggle } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <div className='footer'>
-        <h1>Footer</h1>
-
+        <hr/>
+        <div className='footer-section'>
+          <div className="footer-icons">
+              <a href="https://www.linkedin.com/in/nikendrashekhawat/"><FontAwesomeIcon className='footer-icon' icon={faLinkedin} /></a>
+              <a href="https://www.github.com/nikendrashekhawat"><FontAwesomeIcon className='footer-icon' icon={faSquareGithub} /></a>
+              <a href="https://www.kaggle.com/nikendrashekhawat"><FontAwesomeIcon className='footer-icon' icon={faKaggle} /></a>
+            </div>
+          <div className="footer-copyright">
+            <p>©2025 Nikendra Shekhawat</p>
+          </div>
+        </div>
     </div>
   )
 }
