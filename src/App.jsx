130eea952eb2252b29dda28fa5d4c2baa2d34c39
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-19 17:15:39 -0500

    add refresh to home feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
 import Navbar from './components/navbar/Navbar'
 import Profile from './components/home/Header'
 import About from './components/about/About'
@@ -8,6 +8,30 @@ import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 const App = () => {
+
+  useEffect(() => {
+    const handleRefresh = () => {
+      // Force scroll to top first
+      window.scrollTo(0, 0);
+      // Then set the hash to profile
+      window.location.hash = 'profile';
+    };
+
+    // Run on mount and refresh
+    handleRefresh();
+
+    // Also handle the beforeunload event
+    window.addEventListener('beforeunload', () => {
+      window.location.hash = 'profile';
+    });
+
+    return () => {
+      window.removeEventListener('beforeunload', () => {
+        window.location.hash = 'profile';
+      });
+    };
+  }, []);
+
   return (
     <div>
       <Navbar/>
