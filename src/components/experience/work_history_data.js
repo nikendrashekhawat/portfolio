e461a99fc3316a69d71487e18dbee079ea2f2129
Nikendra Shekhawat <146007305+nikendrashekhawat@users.noreply.github.com>
2025-01-28 23:23:07 -0500

    remove tools from jobs data

--- src/components/experience/work_history_data.js
+++ src/components/experience/work_history_data.js
@@ -5,24 +5,21 @@ const work_history = [
         title: 'IT Support Analyst',
         city: 'SBE Canada Ltd, Mississauga',
         description: "IT Ticket Management, Networking, Hardware & Software Troubleshooting, Security, Azure",
-        date: "Nov 2018 – Sep 2019",
-        tools: ['Windows Server', 'Linux', 'Python', 'BASH', 'CLI', 'ServiceNow', 'Microsoft 365', 'Sharepoint']
+        date: "Nov 2018 – Sep 2019"
     },
     {
         id: 2,
         title: 'Database Analyst',
         city: 'SBE Canada Ltd, Mississauga',
         description: "Database Management, ETL, Data Pipelines, Data Dictionary, Data governance, Data Warehouse, Dashboards",
-        date: "Oct 2019 – Dec 2022",
-        tools: ['Ubuntu Server', 'MySQL Server', 'SQL', 'Python', 'Airflow', 'Spreadsheet', 'Tableau', 'BigQuery', 'DataFlow', 'Cloud SQL', 'Looker'],
+        date: "Oct 2019 – Dec 2022"
     },
     {
         id: 3,
         title: 'BI Analyst',
         city: 'Ivy Technology, North York',
         description: "Data Modeling, EDA, Dashboards, Script Automation, Metrics and KPIs reportings",
-        date: "May 2023 – Feb 2024",
-        tools: ['MySQL','Excel', 'MS Access',  'R', 'Python', 'Pandas', 'Power BI', 'Power Query',]
+        date: "May 2023 – Feb 2024"
     },
     {
         id: 4,
@@ -30,8 +27,7 @@ const work_history = [
         city: 'CPVM Group, Vaughan',
         description: "Sales & Revenue, Customer Segmentation, Market Segmentation, Marketing Campaign, Hypothesis Testing,\
         Sales & Demand Forecasting, Inventory Planning & Forecasting",
-        date: "Mar 2024 – Present",
-        tools: ['Python', 'PostgreSQL', 'Excel', 'Tableau', 'Streamlit', 'Scipy', 'Pandas', 'Scikit-learn', 'Prophet']
+        date: "Mar 2024 – Present"
     }
 ];
 
